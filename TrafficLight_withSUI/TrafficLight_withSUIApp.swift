7ac2abf4d2b3d188ba6344eac7c52a49fab2d379
@Annabel_ <hydrant94@gmail.com>
2023-04-20 22:20:27 +0400

    Initial Commit

---
+++ TrafficLight_withSUI/TrafficLight_withSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_withSUIApp.swift
+//  TrafficLight_withSUI
+//
+//  Created by Аня Беликова on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_withSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
