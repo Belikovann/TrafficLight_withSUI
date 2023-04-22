1c7377cda41a0581b4702f58df0e93f1a586a13e
@Annabel_ <hydrant94@gmail.com>
2023-04-22 23:05:08 +0400

    wip

--- TrafficLight_withSUI/CircleView.swift
+++ TrafficLight_withSUI/CircleView.swift
@@ -7,11 +7,14 @@
 
 import SwiftUI
 
+
 struct CircleView: View {
+
     var body: some View {
         Circle()
             .frame(width: 110, height: 110)
             .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            .opacity(0.3)
     }
 }
 
--- TrafficLight_withSUI/ContentView.swift
+++ TrafficLight_withSUI/ContentView.swift
@@ -7,24 +7,69 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red,
+         yellow,
+         green
+}
+
 struct ContentView: View {
+    
+    @State private var currentLight = CurrentLight.red
+    @State private var redLight = CircleView()
+    @State private var yellowLight = CircleView()
+    @State private var greenLight = CircleView()
+    @State private var lightIsOn: CGFloat = 1
+    
+    
     var body: some View {
         ZStack() {
             Color(.black)
                 .ignoresSafeArea()
             VStack(spacing: 15.0) {
-                CircleView()
+                redLight
                     .foregroundColor(.red)
-                CircleView()
+                yellowLight
                     .foregroundColor(.yellow)
-                CircleView()
+                greenLight
                     .foregroundColor(.green)
             }
-            Button("START") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            .padding(.bottom, 300)
+            
+            Button(action: switchButton) {
+                Text(" START ")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding(.horizontal)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .frame(maxWidth: .greatestFiniteMagnitude)
             }
+            .padding(.top, 600)
         }
-    
+        
+    }
+}
+
+extension ContentView {
+    private func switchButton() {
+        
+        switch currentLight {
+        case .red:
+            _ = greenLight.opacity(0) as! CircleView
+            _ = redLight.opacity(lightIsOn)
+            currentLight = .yellow
+        case .yellow:
+            _ = redLight.opacity(0)
+            _ = yellowLight.opacity(lightIsOn)
+            currentLight = .green
+        case .green:
+            _ = greenLight.opacity(lightIsOn)
+            _ = yellowLight.opacity(0)
+            currentLight = .red
+        }
+        
     }
 }
 
